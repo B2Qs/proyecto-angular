070fbe1d055ff1fa5d6a6b087a1503e081e50662
B2Qs <maronenkoni@gmail.com>
2024-08-31 21:07:55 -0400

    Inicio del proyecto

---
+++ todoList/src/app/add-task-component/add-task-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-task-component.component.html',
+  styleUrl: './add-task-component.component.scss'
+})
+export class AddTaskComponentComponent {
+
+}
---
+++ todoList/src/app/task-list-component/task-list-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-list-component.component.html',
+  styleUrl: './task-list-component.component.scss'
+})
+export class TaskListComponentComponent {
+
+}
